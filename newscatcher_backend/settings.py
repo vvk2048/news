0269115b05d92334939e0a7bd8d9222867633a5b
vvk2048 <vemankarnati5@gmail.com>
2020-12-16 17:16:24 +0530

    first commit

--- newscatcher_backend/settings.py
+++ newscatcher_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -193,3 +195,4 @@ ACCOUNT_USERNAME_REQUIRED = False
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+django_heroku.settings(locals())
